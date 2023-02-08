9d4d5916b6e319dad4a2fedbba6252e9b5c0b708
Diana Basenkova <dinnight123@yandex.ru>
2023-02-08 14:18:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 18123; //"рублей"
+        int miles = 20; //"рублей за 1 милю"
+        int bonus = (price / miles);
+        System.out.println(bonus);
+    }
+}
